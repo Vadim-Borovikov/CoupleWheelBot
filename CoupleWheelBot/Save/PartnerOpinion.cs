86d84e2db9de8e77ba2f6ed7667691278c11ff36
Вадим Боровиков <vadim.borovikov@gmail.com>
2023-12-25 14:19:23 +0100

    Added ctor to PartnerOpinion

--- CoupleWheelBot/Save/PartnerOpinion.cs
+++ CoupleWheelBot/Save/PartnerOpinion.cs
@@ -1,17 +1,22 @@
 ﻿using JetBrains.Annotations;
+// ReSharper disable NullableWarningSuppressionIsUsed
 
 namespace CoupleWheelBot.Save;
 
 public sealed class PartnerOpinion
 {
     [UsedImplicitly]
-    public string UserName { get; set; }
+    public string UserName { get; set; } = null!;
     [UsedImplicitly]
-    public List<byte?> Estimates { get; init; }
+    public List<byte?> Estimates { get; init; } = null!;
 
     internal int NextIndex => Estimates.IndexOf(null);
 
     internal bool Done => Estimates.All(e => e.HasValue);
+
+    [UsedImplicitly]
+    public PartnerOpinion() { }
+
 
     public PartnerOpinion(string name, byte questionsNumber)
     {
